6f887c9b8a7f857a705f6ff6aaa681c9fda429f9
Johann Neuhauser <jneuhauser@dh-electronics.com>
2022-11-02 06:55:43 -0700

    lab-taxi: Implement expected sarsa

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -10,8 +10,17 @@ class Agent:
         ======
         - nA: number of actions available to the agent
         """
+        self.alpha = 1.0
+        self.epsilon = 0.005
+        self.gamma = 1.0
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        
+    def get_epsilon_greedy_probs(self, state):
+        best_a = np.argmax(self.Q[state])
+        policy_s = np.ones(self.nA) * self.epsilon / self.nA
+        policy_s[best_a] = 1 - self.epsilon + (self.epsilon / self.nA)
+        return policy_s
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +33,13 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        # get action probabilities with epsilon greedy strategy
+        policy_s = self.get_epsilon_greedy_probs(state)
+        
+        # select random action based on policy for current state
+        action = np.random.choice(np.arange(self.nA), p=policy_s)
+        
+        return action
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +52,9 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        self.Q[state][action] = self.Q[state][action] + self.alpha * (
+            reward + self.gamma * np.dot(
+                self.Q[next_state],
+                self.get_epsilon_greedy_probs(next_state)
+            ) - self.Q[state][action]
+        )
